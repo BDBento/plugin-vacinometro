76aca0d6a163f2b7a2cb32995ab06ad503b3128b
bruno bento <66705552+BDBento@users.noreply.github.com>
2021-06-24 23:42:38 -0400

    Add files via upload

---
+++ -shortcode/vac-sgi.php
@@ -0,0 +1,100 @@
+<?php
+/*
+Plugin Name:       Vac-sgi
+Plugin URI:        
+Description:       Vacinometro
+Version:           1.0.0
+Requires at least: 5.2
+Requires PHP:      7.2
+Author:            bbento
+Author URI:        
+License:           
+License URI:       
+Text Domain:       Vac-sgi
+Domain Path:       
+*/
+if(!defined('ABSPATH'))exit;
+
+function vac_style(){
+
+wp_enqueue_style( 'vac-style',plugins_url('/public/css/vac-style.css',__FILE__));
+
+}
+add_action( 'wp_enqueue_scripts', 'vac_style' );
+
+/*
+function register_vacinometro(){
+    register_widget('vacinometro');
+}
+
+*/
+
+class novo_vacinometro{
+    private static $instace;
+
+    public static function getVacinometro(){
+        if(self::$instace == NULL){
+            self::$instace == new self();
+        }
+        return self::$instace;
+    }
+    private function __construct(){
+        add_shortcode('vacinometro',array($this,'vacinometro'));
+    }
+
+
+    public function vacinometro()
+    {
+        ?>
+
+
+        <div class="vac-titulo">
+        
+                <p>Vacinômetro </p>
+        </div>
+
+                <div class="vacinometro">
+
+                    <div class="vac-interno">
+
+                        <div class="part-1">
+                            
+                                <p class="txt-atualizado">Atualizado Em:</p>
+                            
+
+                            <div class="tempo">
+                                
+                                    <iframe class="atualizado-em" src="https://mais.saude.ms.gov.br:444/single/?appid=d3edb27b-8f52-4dff-a2e0-fc82bfc3fb6a&obj=fRdVtG&theme=hayward's%20paul%20alberto&opt=nointeraction,noselections"></iframe>
+                            
+                            </div>
+                        </div>
+
+                        <div class="part-2">
+                            <p >Doses Aplicadas </p>
+
+                                <div class="doses">
+                                    
+                                        <iframe src="https://mais.saude.ms.gov.br:444/single/?appid=d3edb27b-8f52-4dff-a2e0-fc82bfc3fb6a&obj=RGxrv&opt=nointeraction,noselections"></iframe>
+                                
+                                </div>
+                        </div>           
+                        
+                        <div class="part-3">                            
+                                <p>Percentual das pessoas de MS</br> que ja receberam a segunda dose</p>
+                            
+                                <div class="porcentagem">    
+                        
+                                    <iframe src="https://mais.saude.ms.gov.br:444/single/?appid=d3edb27b-8f52-4dff-a2e0-fc82bfc3fb6a&obj=wMcDP&opt=nointeraction,noselections"></iframe>
+                                    
+                                </div>
+
+                        </div>
+
+                    </div>
+                </div>
+                
+<?php
+    }
+}
+
+novo_vacinometro::getVacinometro();
